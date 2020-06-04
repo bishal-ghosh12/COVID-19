4397883a724287dd8669eb77e0344b9ef71367e4
Bishal Ghosh <Bishalg@Bishal>
2020-06-04 18:16:23 +0530

    Updates

---
+++ precache-manifest.31a0c2c9671a604fb78cf3e258fa0278.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "22a969b922a88b9d3029e1d29ec78f79",
+    "url": "/COVID-19/index.html"
+  },
+  {
+    "revision": "f94710813627c1baf3c7",
+    "url": "/COVID-19/static/css/main.83596afb.chunk.css"
+  },
+  {
+    "revision": "429cbc07020f224db0cc",
+    "url": "/COVID-19/static/js/2.a1fef6d9.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/COVID-19/static/js/2.a1fef6d9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f94710813627c1baf3c7",
+    "url": "/COVID-19/static/js/main.bd24a5a6.chunk.js"
+  },
+  {
+    "revision": "2db74e706bd864c624a0",
+    "url": "/COVID-19/static/js/runtime-main.1514f030.js"
+  },
+  {
+    "revision": "a84eb560fc148eeec4a6ec2349466177",
+    "url": "/COVID-19/static/media/CORONA1.a84eb560.jpg"
+  }
+]);
